53cc79d0accf9c748a810574b9a23f173614d5d3
Ivan Simon <ivansimon@Ivans-MacBook-Pro.local>
2024-05-10 23:21:37 +0200

    modify 1.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // You can choose any port number
 
 app.get('/*', (req, res) => {
-  res.send('Hello World!');
+  res.send('g6x9UrW4FkGkirzXZ7o2kxRHaz6_N6jZNN32LGUrfBw.2biBbcPIT1WpvT2R4cVF7RbunMmuwFubkqQGA9bH6JE');
 });
 
 app.listen(port, () => {
