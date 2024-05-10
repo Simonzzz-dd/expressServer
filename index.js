51f2f57bb30d3e9ca5fcd0ceb2dbeec96846193e
Ivan Simon <ivansimon@Ivans-MacBook-Pro.local>
2024-05-10 23:27:22 +0200

    modify 1.2

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any port number
 
-app.get('/*', (req, res) => {
-  res.send('g6x9UrW4FkGkirzXZ7o2kxRHaz6_N6jZNN32LGUrfBw.2biBbcPIT1WpvT2R4cVF7RbunMmuwFubkqQGA9bH6JE');
+// Define the data and path
+const data = 'yxOxmhIdz3OUS4QivU1HI_J8MGmD5KU_cnkOki7vPws.2biBbcPIT1WpvT2R4cVF7RbunMmuwFubkqQGA9bH6JE';
+const challengePath = '/.well-known/acme-challenge/yxOxmhIdz3OUS4QivU1HI_J8MGmD5KU_cnkOki7vPws';
+
+// Serve the data at the specified path
+app.get(challengePath, (req, res) => {
+    res.type('text/plain');
+    res.send(data);
 });
-
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
